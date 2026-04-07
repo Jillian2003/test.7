411ec3ecb4231a6ecc0f6c02a73a3b5ab3fb325d
Jillian Ireland <141571184+Jillian2003@users.noreply.github.com>
2026-04-07 13:52:13 -0400

    Delete moving directory

--- moving/alumni-backend/db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config(); // loads .env
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ Connected to MongoDB!'))
-  .catch(err => console.error('❌ MongoDB connection error:', err));
-
-module.exports = mongoose;
